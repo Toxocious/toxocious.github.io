963f83815c67ac867c6b53546ca453056ec38d91
Jess <toxocious@gmail.com>
2022-09-10 20:41:06 -0700

    Display a random quote in the banner

--- src/components/header/header.tsx
+++ src/components/header/header.tsx
@@ -1,11 +1,28 @@
-import { CONFIGURATION } from '../../config';
+import { useEffect, useState } from 'react';
+
+import { QUOTES, QuoteType } from '../../constants/quotes';
 
 import './header.scss';
 
 export const Header = () => {
+  const [quote, setQuote]: [QuoteType, any] = useState({
+    quote: '',
+    author: '',
+  });
+
+  useEffect(() => {
+    const RANDOM_QUOTE = QUOTES[Math.floor(Math.random() * QUOTES.length)];
+
+    setQuote(RANDOM_QUOTE);
+  }, [quote]);
+
   return (
     <header>
-      <div className='header_container'></div>
+      <div className='header_container'>
+        <h3>
+          "{quote.quote}" &mdash; {quote.author}
+        </h3>
+      </div>
     </header>
   );
 };
---
+++ src/constants/quotes.ts
@@ -0,0 +1,73 @@
+export type QuoteType = {
+  quote: string;
+  author: string;
+};
+
+export const QUOTES: Array<QuoteType> = [
+  {
+    quote:
+      "I'm not a great programmer; I'm just a good programmer with great habits",
+    author: 'Kent Beck',
+  },
+  {
+    quote:
+      'Good programmers know what to write. Great ones know what to rewrite and reuse',
+    author: 'Eric S. Raymond',
+  },
+  {
+    quote:
+      'Any fool can write code that a computer can understand. Good programmers write code that humans can understand.',
+    author: 'Martin Fowler',
+  },
+  {
+    quote:
+      'Good programmers use their brains, but good guidelines save us having to think out every case.',
+    author: 'Francis Glassborow',
+  },
+  {
+    quote:
+      'Common programmer thought pattern: there are only three numbers: 0, 1, and n.',
+    author: 'Joel Spolsky',
+  },
+  {
+    quote:
+      "A language that doesn't affect the way you think about programming is not worth knowing",
+    author: 'Alan Perlis',
+  },
+  {
+    quote:
+      'One of my most productive days was throwing away 1,000 lines of code.',
+    author: 'Ken Thompson',
+  },
+  {
+    quote: 'The wages of sin is debugging.',
+    author: 'Ron Jeffries',
+  },
+  {
+    quote: 'Any program is only as good as it is useful.',
+    author: 'Linus Torvalds',
+  },
+  {
+    quote:
+      'The function of good software is to make the complex appear to be simple',
+    author: 'Grady Booch',
+  },
+  {
+    quote: 'With enough eyes, all bugs are shallow.',
+    author: 'Eric S. Raymond',
+  },
+  {
+    quote:
+      'Data is a precious thing and will last longer than the systems themselves.',
+    author: 'Tim Berners-Lee',
+  },
+  {
+    quote: "It's really complex to make something simple.",
+    author: 'Jack Dorsey',
+  },
+  {
+    quote:
+      'Optimism is an occupational hazard of programming; feedback is the treatment.',
+    author: 'Kent Beck',
+  },
+];
