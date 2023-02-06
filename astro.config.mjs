57caaf7c90ec13c1e71754c7318307b1cb88759b
Jess <toxocious@gmail.com>
2023-02-06 14:07:26 -0800

    feat: add syntax highlighting to code blocks in md files

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,9 +1,11 @@
 import { defineConfig } from 'astro/config';
 
-// https://astro.build/config
 export default defineConfig({
   site: 'https://toxocious.is-a.dev/',
   markdown: {
-    syntaxHighlight: 'prism',
-  }
+    shikiConfig: {
+      theme: 'dracula',
+      wrap: true,
+    },
+  },
 });
