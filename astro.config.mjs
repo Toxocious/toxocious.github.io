0d14686e87f4a021d42983b807522e0a32346667
Jess <toxocious@gmail.com>
2023-04-15 12:53:18 -0700

    upload overwritten config files and w/e

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,13 +1,6 @@
 import { defineConfig } from 'astro/config';
 
-import react from "@astrojs/react";
-import tailwind from "@astrojs/tailwind";
-
 export default defineConfig({
-  integrations: [
-    react(),
-    tailwind()
-  ],
   site: 'https://toxocious.is-a.dev/',
   markdown: {
     shikiConfig: {
