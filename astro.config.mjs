a48db540ab43d820a8cdcf3fc5a4cc3960dd41c6
Jess <toxocious@gmail.com>
2023-04-15 12:54:43 -0700

    fix astro configuration

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,7 +1,14 @@
 import { defineConfig } from 'astro/config';
 
+import react from "@astrojs/react";
+import tailwind from "@astrojs/tailwind";
+
 export default defineConfig({
   site: 'https://toxocious.is-a.dev/',
+  integrations: [
+    react(),
+    tailwind()
+  ],
   markdown: {
     shikiConfig: {
       theme: 'dracula',
