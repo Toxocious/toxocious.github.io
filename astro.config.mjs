0c170bd540389e9a81ebfd95f371797f6ce58f2f
Toxocious <toxocious@gmail.com>
2023-09-01 22:05:29 -0700

    chore: remove react and sass dependencies

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,11 +1,10 @@
 import { defineConfig } from 'astro/config';
 
-import react from '@astrojs/react';
 import tailwind from '@astrojs/tailwind';
 
 export default defineConfig({
     site: 'https://toxocious.is-a.dev/',
-    integrations: [react(), tailwind()],
+    integrations: [tailwind({ configFile: './tailwind.config.cjs' })],
     markdown: {
         shikiConfig: {
             theme: 'dracula',
