85ef0e03e36c088f7c0e97cfd542933663bdedd0
Toxocious <toxocious@gmail.com>
2023-09-02 21:48:35 -0700

    feat: add plugin to estimate markdown read time

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,11 +1,13 @@
 import { defineConfig } from 'astro/config';
-
 import tailwind from '@astrojs/tailwind';
+
+import { remarkReadingTime } from './markdown-eta.mjs';
 
 export default defineConfig({
     site: 'https://toxocious.is-a.dev/',
     integrations: [tailwind({ configFile: './tailwind.config.cjs' })],
     markdown: {
+        remarkPlugins: [remarkReadingTime],
         shikiConfig: {
             theme: 'dracula',
             wrap: true,
