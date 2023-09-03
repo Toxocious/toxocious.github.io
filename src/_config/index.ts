855bf7d7c5b0fa590d1f18820c61cdb9f4386b57
Toxocious <toxocious@gmail.com>
2023-09-02 20:44:06 -0700

    feat: update user config file

--- src/_config/index.ts
+++ src/_config/index.ts
@@ -1,28 +1,37 @@
 export const config = {
-  name: 'Jesse Mack',
-  occupation: 'Software Engineer',
-  location: 'Washington, United States',
+    name: 'Jesse Mack',
+    occupation: 'Software Engineer',
+    location: 'Arizona, United States',
 
-  github: 'toxocious',
-  gitlab: '',
-  linked_in: 'mackjesse',
-  twitter: 'toxocious',
-  email: 'mackjessed@gmail.com',
+    github: 'toxocious',
+    gitlab: '',
+    linked_in: 'mackjesse',
+    twitter: 'toxocious',
+    email: 'mackjessed@gmail.com',
 
-  resume: 'Jesse_Mack_Software_Engineer_Resume.pdf',
+    resume: 'Jesse_Mack_Software_Engineer_Resume.pdf',
 
-  summary: "I specialize in PHP and TypeScript. Through years of hands-on experience, I've cultivated a deep understanding of these technologies and their nuances. This expertise allows me to architect backend systems using PHP for robust data processing, and frontend interfaces using TypeScript for enhanced user interactions, resulting in high-performance applications.",
+    get summary() {
+        return `Hey there! I'm ${config.name} from ${config.location}, and I love to make beautiful things, solve complex puzzles, and brainstorm new ideas.`;
+    },
+    get headline() {
+        return `${this.occupation} and aspiring entrepreneur`;
+    },
 
-  seo: {
-    keywords: 'toxocious, jesse mack, software developer, software engineer, php, react, reactjs, node, nodejs, html, css, git, github',
+    seo: {
+        keywords:
+            'toxocious, jesse mack, software developer, software engineer, php, sql, mysql, node, nodejs, html, css, git, github',
 
-    description: "Jesse Mack's personal portfolio as a software engineer.",
+        get description() {
+            return config.summary;
+        },
 
-    site_type: 'blog',
+        site_type: 'website',
 
-    site_url: 'https://toxocious.is-a.dev/',
+        site_url: 'https://toxocious.is-a.dev/',
 
-    twitter_card_image: "images/twitter_card.png",
-    twitter_card_description: "Jesse Mack's personal portfolio as a software engineer.",
-  }
+        twitter_card_image: 'images/twitter_card.png',
+        twitter_card_description:
+            "Jesse Mack's personal portfolio as a software engineer.",
+    },
 };
